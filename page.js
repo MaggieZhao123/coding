09b0946e91757829f841f38ef813cbbbfbd66538
赵一涵 <zhaoyihan@MZMacBook-Pro.local>
2023-06-01 16:48:11 +1200

    Maggie upadate

---
+++ page.js
@@ -0,0 +1,44 @@
+function setup() {
+  const canvasDiv = document.getElementById('canvasDiv');
+  const canvas = createCanvas(350,350,WEBGL);
+  canvas.parent(canvasDiv);
+}
+
+function draw() {
+  background('#515f41');
+
+  
+  normalMaterial();
+ 
+
+
+  push();
+  rotateZ(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(150, 150, 150);
+  pop();
+
+ 
+}
+
+function canvasHover(){
+  const popWindow = document.getElementById("popWindow");
+  popWindow.classList.remove("hidden");
+}
+
+function canvasHoverLeave(){
+  const popWindow = document.getElementById("popWindow");
+  popWindow.classList.add("hidden");
+}
+
+function imgHover(imgId){
+  const imgDiv = document.getElementById(imgId);
+  imgDiv.classList.remove("hidden");
+}
+
+function imgHoverLeave(imgId){
+  const imgDiv = document.getElementById(imgId);
+  imgDiv.classList.add("hidden");
+
+}
